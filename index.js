b58fd913126412b2ad56b00a286f1ba16960605b
Cheng-Dar Chiang <tboydar@gmail.com>
2018-03-27 00:23:42 +0800

    fix listen

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ app.use(router.routes());
 
 // app.listen(80);
 app.listen(process.env.PORT || 3000, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log("Express server listening on port %d in  mode", this.address().port);
 });
