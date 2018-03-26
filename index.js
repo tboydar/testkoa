627b783e63854390773a6d5657c2bbb8cd0aa0dd
Cheng-Dar Chiang <tboydar@gmail.com>
2018-03-27 00:03:00 +0800

    init2

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const Koa = require('koa')
-const app = new Koa()
+const Koa = require('koa');
+const Router = require('koa-router');
 
-app.use(async(ctx) => {
-    ctx.body = 'hello koa2'
-})
+const app = new Koa();
+const router = Router();
 
-app.listen(3000)
-console.log('[demo] start-quick is starting at port 3000')
+// 設定根路徑的處理函數
+router.get('/', async function(ctx) {
+    ctx.body = 'Hello World / ';
+});
+
+router.get('/api/user', async function(ctx) {
+
+    // 檢查 Token，若有問題回傳 400 HTTP StatusCode
+    if (ctx.query.token == '123')
+        ctx.throw(400);
+
+    // 若已經拋出 400 的狀態，接下來的程式不會被執行
+    ctx.body = 'Hello World ' + '/api/user';
+});
+
+app.use(router.routes());
+
+app.listen(3000);
